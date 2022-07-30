e15340f6bf7b5ab65e1af64a3a89b24b429c9143
nicolas <nicolas.goulartsouza123@gmail.com>
2022-07-29 21:19:10 -0300

    Update Main-Home-Responsive

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -11,8 +11,10 @@ export default function Home(){
         <S.Left>
           <S.TitleBox>Ajudar você a encontrar o melhor conforto, é nossa prioridade.</S.TitleBox>
           <S.Text>Encontre uma variedade de propriedades que combinam com você. Esqueça todas as dificuldades em encontrar uma residência</S.Text>
+          <S.DivButtons>
             <S.Button>Entre em contato</S.Button>
             <S.Link href="#">Mais sobre nós</S.Link>
+          </S.DivButtons>
         </S.Left>
         <S.Image src={HouseHome} alt="Imagem de uma casa"/>
       </S.HomeSection>
--- src/components/main/home/Styles.js
+++ src/components/main/home/Styles.js
@@ -5,15 +5,36 @@ export const HomeSection = styled.section`
   align-items: center;
   justify-content: space-between;
   height: 900px;
+  @media(max-width: 900px){
+    height: 400px;
+    justify-content: center;
+  }
 `
 export const Left = styled.div`
   width: 40vw;
+  @media(max-width: 900px){
+    width: 80%;
+    border: solid;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media(max-width: 500px){
+    width: 95%;
+  }
 `
 export const TitleBox = styled.h1`
   font-size: 3rem;
   font-weight: 600;
   font-family: 'Montserrat', sans-serif;
   color: #272727;
+  @media(max-width: 1100px){
+    font-size: 2rem;
+  }
+  @media(max-width: 900px){
+    text-align: center; 
+    font-size: 1.7rem;
+  }
 `
 export const Text = styled.p`
   margin-top: 20px;
@@ -22,6 +43,17 @@ export const Text = styled.p`
   font-family: 'Montserrat', sans-serif;
   color: #797979;
   width: 30vw;
+  @media(max-width: 1100px){
+    font-size: 1rem;
+  }
+  @media(max-width: 900px){
+    width: 100%;
+    text-align: center; 
+    font-size: 0.8rem;
+  }
+`
+export const DivButtons = styled.div`
+  
 `
 export const Button = styled.button`
   margin-top: 20px;
@@ -37,6 +69,11 @@ export const Button = styled.button`
     cursor: pointer;
     color: grey;
   }
+  @media(max-width: 1100px){
+    font-size: 1rem;
+    width: 160px;
+    height: 40px;
+  }
 `
 export const Link = styled.a`
   margin-left: 50px;
@@ -47,6 +84,10 @@ export const Link = styled.a`
   &:hover{
     color: grey;
   }
+  @media(max-width: 1100px){
+    margin-left: 30px;
+    font-size: 1rem;
+  }
 `
 export const Image = styled.img`
   width: 700px;
@@ -54,5 +95,8 @@ export const Image = styled.img`
   @media(max-width: 1440px){
     width: 500px;
     height: 700px;
+  }
+  @media(max-width: 900px){
+    display: none;
   }
 `
