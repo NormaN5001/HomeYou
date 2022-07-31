621e749e66ca68c8f7753d73483c4af14b12db7a
nicolas <nicolas.goulartsouza123@gmail.com>
2022-07-31 18:00:55 -0300

    Update Main-HomeTwo-Responsive

--- src/components/main/home/Home.js
+++ src/components/main/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import * as S from './Styles'
 
-import HouseHome from '../../../img/house1.png'
+import HouseHome1 from '../../../img/house1.png'
+import HouseHome2 from '../../../img/house2.png'
 
 export default function Home(){
 
   return(
     <>
-      <S.HomeSection>
+      <S.HomeOne>
         <S.Left>
           <S.TitleBox>Ajudar você a encontrar o melhor conforto, é nossa prioridade.</S.TitleBox>
           <S.Text>Encontre uma variedade de propriedades que combinam com você. Esqueça todas as dificuldades em encontrar uma residência</S.Text>
@@ -16,8 +17,18 @@ export default function Home(){
             <S.Link href="#">Mais sobre nós</S.Link>
           </S.DivButtons>
         </S.Left>
-        <S.Image src={HouseHome} alt="Imagem de uma casa"/>
-      </S.HomeSection>
+        <S.HouseImg1 src={HouseHome1} alt="Imagem de uma casa"/>
+      </S.HomeOne>
+
+      <S.HomeTwo>
+        <S.BackgroundImg>
+          <S.HouseImg2 src={HouseHome2} alt="Imagem do interior de uma casa"/>
+        </S.BackgroundImg>
+        <S.Right>
+          <S.TitleBoxTwo>Design Minimalista</S.TitleBoxTwo>
+          <S.TextTwo>Móveis apenas essenciais e com design moderno, além de revestimentos bonitos e em cores neutras para manter a casa sempre com um estilo clean e suave.</S.TextTwo>
+        </S.Right>
+      </S.HomeTwo>
     </>
   )
 }
--- src/components/main/home/Styles.js
+++ src/components/main/home/Styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const HomeSection = styled.section`
+export const HomeOne = styled.section`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -14,7 +14,6 @@ export const Left = styled.div`
   width: 40vw;
   @media(max-width: 900px){
     width: 80%;
-    border: solid;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -89,7 +88,7 @@ export const Link = styled.a`
     font-size: 1rem;
   }
 `
-export const Image = styled.img`
+export const HouseImg1 = styled.img`
   width: 700px;
   height: 900px;
   @media(max-width: 1440px){
@@ -98,5 +97,81 @@ export const Image = styled.img`
   }
   @media(max-width: 900px){
     display: none;
+  }
+`
+export const HomeTwo = styled.section`
+  margin-top: 180px;
+  display: flex;
+  justify-content: space-between;
+  @media(max-width: 900px){
+    margin-top: 50px;
+    justify-content: center;
+  }
+`
+export const BackgroundImg = styled.div`
+  background-color: #272727;
+  width: 487px;
+  height: 457px;
+  position: relative;
+  @media(max-width: 1440px){
+    width: 430px;
+    height: 390px;
+  }
+  @media(max-width: 1080px){
+    background-color: transparent;
+  }
+  @media(max-width: 900px){
+    display: none;
+  }
+`
+export const HouseImg2 = styled.img`
+  position: absolute;
+  left: 50px;
+  top: 50px;
+  width: 100%;
+  height: 100%;
+  @media(max-width: 1080px){
+    left: 0;
+    top: 0;
+  }
+`
+export const Right = styled.div`
+  background-color: #272727;
+  width: 585px;
+  height: 182px;
+  padding: 20px 0 0 20px;
+  @media(max-width: 1440px){
+    width: 460px;
+  }
+  @media(max-width: 900px){
+    width: 80%;
+    height: 140px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  @media(max-width: 500px){
+    width: 95%;
+  }
+`
+export const TitleBoxTwo = styled.h2`
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #FFF;
+  @media(max-width: 1440px){
+    font-size: 1.4rem;
+  }
+`
+export const TextTwo = styled.p`
+  margin-top: 20px;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1rem;
+  font-weight: 500;
+  color: #FFF;
+  @media(max-width: 900px){
+    width: 100%;
+    text-align: center; 
+    font-size: 0.8rem;
   }
 `
